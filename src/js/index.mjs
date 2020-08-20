acd2cd262fe6baaf2d91dacfc17cea68a38065e3
kimjouny <juny4500@naver.com>
2020-08-21 01:10:56 +0900

    [FEAT] implement spend simulator

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -51,3 +51,95 @@ new Chart(ctx, {
     }
   }
 });
+
+const SPEND_OPTIONS=document.getElementsByClassName('chartJS_spend_container')[0];
+SPEND_OPTIONS.addEventListener('click',(e)=>{
+  switch(e.target.tagName){
+    case 'LI':{
+      const SPEND_INPUT=document.getElementsByClassName('spend_input')[0];
+      SPEND_INPUT.innerHTML=e.target.dataset.spend;
+      /*change color */
+      const ACTIVED=document.getElementsByClassName('spend_selected')[0];
+      if(ACTIVED===e.target)break;
+      ACTIVED.classList.remove('spend_selected');
+      e.target.classList.add('spend_selected');
+      /*change horizontal height */
+      changeHorozontal(e.target.dataset.spend);
+      break;
+    }
+    case 'DIV':{
+      if(e.target.classList[0]=="spend_close_btn"){
+        const SPEND_BUTTON=document.getElementsByClassName('edit_spend')[0];
+        SPEND_OPTIONS.style.display='none';
+        SPEND_BUTTON.classList.remove('edit_selected')
+        SPEND_BUTTON.style.color='white'
+        SPEND_BUTTON.style.border='none'
+      }
+      // hide component;
+      break;
+    }
+    default :{}
+  }
+});
+
+const changeHorozontal=(spendInput)=>{
+  const SPEND_LINE=document.getElementsByClassName('chartJS_spendline')[0];
+  const SPEND_BOX=document.getElementsByClassName('chartJS_spendtext')[0];
+  switch(spendInput){
+    case '170':{
+      SPEND_LINE.style.top='60%';
+      SPEND_BOX.style.top='60%';
+      break;
+    }
+    case '210':{
+      SPEND_LINE.style.top='50%';
+      SPEND_BOX.style.top='50%';
+      break;
+    }
+    case '240':{
+      SPEND_LINE.style.top='45%';
+      SPEND_BOX.style.top='45%';
+      break;
+    }
+    case '265':{
+      SPEND_LINE.style.top='40%';
+      SPEND_BOX.style.top='40%';
+      break;
+    }
+    case '360':{
+      SPEND_LINE.style.top='30%';
+      SPEND_BOX.style.top='30%';
+      break;
+    }
+    default:{}
+  }
+}
+
+const EDIT_BUTTONS=document.getElementsByClassName('chartJS_edit_container')[0];
+EDIT_BUTTONS.addEventListener('click',(e)=>{
+  if(e.target.classList.length==3){
+    e.target.classList.remove('edit_selected')
+    e.target.style.color='white'
+    e.target.style.border='none'
+    if(e.target.classList[1]=='edit_spend'){
+      document.getElementsByClassName('chartJS_spend_container')[0].style.display='none';
+    }
+    return;
+  }
+  editUnselectAll()
+  e.target.classList.add('edit_selected');
+  e.target.style.color=e.target.dataset.color
+  e.target.style.border=`1px solid ${e.target.dataset.color}`
+  if(e.target.classList[1]=='edit_spend'){
+    document.getElementsByClassName('chartJS_spend_container')[0].style.display='flex';
+  }
+})
+
+const editUnselectAll=()=>{
+  const BUTTONS=EDIT_BUTTONS.getElementsByClassName('chartJS_edit');
+  for(let i=0; i<BUTTONS.length;i++){
+    BUTTONS[i].classList.remove('edit_selected');
+    BUTTONS[i].style.color='white'
+    BUTTONS[i].style.border=`none`
+  }
+}
